d471491a5cbe3998bd0d8073f5a6b0c6cf7fe775
ModernPancakes <78776670+ModernPancakes@users.noreply.github.com>
2022-02-25 14:54:17 -0800

    JS: typo-fix

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http')
 const port = 3000
 
-const server = http.createServer(function(req, res){
-    Response.write('Hello Node')
+const server = http.createServer(function(req, res) {
+    res.write('Hello Node')
     res.end()
 })
 server.listen(port, function(error) {
